57e2995ef62925daaeb3d4510e427f70d58d966f
Dicky_asqaelani26 <93518546+Dickybulin26@users.noreply.github.com>
2022-12-30 17:11:45 +0700

    Delete uji coba directory

--- uji coba/ujicoba20-boolean/boolean.py
+++
@@ -1,5 +0,0 @@
-A = 10
-B = 10
-C = 20
-print("apakah nilai A == nilai B =",A == B)
-print("apakah nilai B == nilai C =",B == C)
--- uji coba/ujicoba21-percabangan/doni.py
+++
@@ -1,8 +0,0 @@
-nama = "doni"
-
-if nama == "doni":
-    print('DONI GANTENG BANGET!!!!!')
-else: 
-    print('DONI JELEK BANGET!')
-    
-print("TERIMA KASIH YA DONI!!!")
